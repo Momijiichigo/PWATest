2937b06451cefb8e265c37d44bb5a0c15c29c03d
Momijiichigo <ichigomomiji436@gmail.com>
2019-10-11 17:34:12 -0700

    aaa

--- sw.js
+++ sw.js
@@ -1,6 +1,39 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        
-    )
+var CACHE_NAME = "0.0.9";
+const FILES_TO_CACHE = [
+    './index.html',
+    //'./manifest.json',
+    //'./momiji.PNG',
+    './drawing.svg'
+];
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache)=> {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
+
+self.addEventListener('activate', function(event) {  
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+              if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+              }
+            }));
+          })
+      
+    );
+  });
+  self.addEventListener('fetch', function(evt) {
+    evt.respondWith(
+        caches.match(evt.request).then(res=>{
+          if(res)return res;
+          return fetch(evt.request);
+        })
+    );
+  });
