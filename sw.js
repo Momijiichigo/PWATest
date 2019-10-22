a930c34d701e18d9a5c9d0a97cdf3beed30be006
Momijiichigo <ichigomomiji436@gmail.com>
2019-10-21 19:02:07 -0700

    aa

--- sw.js
+++ sw.js
@@ -3,37 +3,43 @@ const FILES_TO_CACHE = [
     './index.html',
     './drawing.svg'
 ];
-
+const mainWin=null;
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
     .then((cache)=> {
         console.log('[ServiceWorker] Pre-caching offline page');
+        if(mainWin!=null)mainWin.postMessage("installing");
         return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
 self.addEventListener('activate', function(event) {  
-    event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-              if (key !== CACHE_NAME) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                return caches.delete(key);
-              }
-            }));
-          })
-      
-    );
-  });
-  self.addEventListener('fetch', function(evt) {
-    evt.respondWith(
-        caches.match(evt.request).then(res=>{
-          return res || fetch(evt.request);
+  event.waitUntil(
+      caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
         })
-    );
-  });
+    
+  );
+});
+self.addEventListener('fetch', function(evt) {
+  evt.respondWith(
+      caches.match(evt.request).then(res=>{
+        return res || fetch(evt.request);
+      })
+  );
+});
+self.addEventListener('message', function(event){
+  console.log("SW Received Message: " + event.data);
+  mainWin=event.ports[0];
+});
+
   /*
 s = ["72x72","96x96", "128x128", "144x144", "152x152", "192x192", "384x384", "512x512"]
 for(x=0;x<s.length;x++){
